36f6ec94bdb816db6b7305373fa88eb49833f8b2
GeJonathan <88726982+GeJonathan@users.noreply.github.com>
2021-08-17 11:18:56 -0300

    js, css, htm

---
+++ proyecto/script.js
@@ -0,0 +1,17 @@
+let btnComponentes = document.getElementById('btnComponentes');
+let btnArmado = document.getElementById('btnArmado');
+
+
+btnArmado.onClick = function clickArmado(){
+
+  document.btnArmado.style.background = "red";
+
+}
+
+
+function clickArmado(){
+
+  document.btnArmado.style.background = "red";
+
+
+}
